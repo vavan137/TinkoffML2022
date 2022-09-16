ca2397cbfe33cdb8818ff2bd9497d2fcf17d85e6
vavan137 <113305894+vavan137@users.noreply.github.com>
2022-09-16 21:27:30 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,14 @@
+import argparse
+
+#--input-dir` путь к директории, в которой лежит коллекция документов. Если данный аргумент не задан, считать, что тексты вводятся из stdin.
+#--model`  путь к файлу, в который сохраняется модель.
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-dir',  type=str)
+parser.add_argument('--model', type=str, required=True)
+args = parser.parse_args()
+####
+
 f = open('../TinkoffML2022/data/Era_Miloserdiya.txt', 'r', encoding="utf8")
 lst2 = []
 for s in f:
@@ -11,7 +22,4 @@ for s in f:
     lst2 = lst2 + [s]
 
 
-#Параметры `train.py`:
 
-#--input-dir` путь к директории, в которой лежит коллекция документов. Если данный аргумент не задан, считать, что тексты вводятся из stdin.
-#--model`  путь к файлу, в который сохраняется модель.
