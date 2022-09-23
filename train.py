51099d48681ee9cc067c4c133a7a411951c29ce7
vavan137 <113305894+vavan137@users.noreply.github.com>
2022-09-23 22:19:03 +0300

    Update train.py

--- train.py
+++ train.py
@@ -2,67 +2,98 @@ import argparse
 import re
 import pickle
 
-#--input-dir` путь к директории, в которой лежит коллекция документов. Если данный аргумент не задан, считать, что тексты вводятся из stdin.
+class NgramModel:
+    def fit(dir='../TinkoffML2022/data/detective/Era_Miloserdiya.txt', model_path='../TinkoffML2022/model.pickle'):
+        #
+        f = open(str(dir), 'r', encoding="utf8")
+        #список предложений
+        lst_snt = []
+        for s in f:
+            # Убрать последний символ '\n' из s
+            s = s.rstrip().lower()
+            if s!='':
+                snt = s.split('.')
+
+                #print("s = ", s)
+                # Добавить строку s в список lst2
+                lst_snt = lst_snt + [snt]
+        
+        output = []
+        outcome = {}
+    
+        for text in lst_snt:
+            text = str(text)
+
+        #text = '- у тебя оружие с собой? у меня оружие при мне, а у тебя.'
+        # генерирую список с n-граммами до 6
+            text = re.sub(r'[^а-яА-Я0-9\s]', ' ', text)
+            words = text.split()
+    
+            for WordsToCombine in range(1,7,1):
+                for i in range(len(words)- WordsToCombine+1):
+                    output.append(words[i:i+WordsToCombine])
+    
+        for k in output:
+            #print('k= ', k)
+            partial_outcome = output.count(k)
+            #print('freq=',partial_outcome)
+            s = ' '.join([str(n) for n in k])
+            LengthPrefix = len(k)
+            res=[]
+            for k2 in output:
+                if len(k2)== LengthPrefix+1:
+                    if s == ' '.join([str(n) for n in k2[:LengthPrefix]]):
+                        #print(k2[LengthPrefix:])
+                        #print(k2[-1])
+                        partial_outcome1 = output.count(k2)
+                        #print('freq=',partial_outcome1)
+                        if (k2[-1], partial_outcome1/partial_outcome) not in res:
+                            res.append((k2[-1], partial_outcome1/partial_outcome))
+            outcome[s] = res
+            #print(res)
+
+        #print(outcome)       
+        with open(str(model_path), 'wb') as handle:
+            pickle.dump(outcome, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+            def predict(model_path, prefix, the_length):
+                with open(str(model_path), 'rb') as handle:
+                    Model = pickle.load(handle)
+                #читаем model.pcl
+                #prefix = str(args.prefix)
+                #the_length= int(args.length)
+                s_out=[]
+
+                for ix in range(the_length):
+                    x = Model.get(prefix)
+                    #print(x)
+                # берем первый по порядку появления в тексте вариант
+                    if x==None:
+                        s_out.append('42')
+                        #print("42")
+                        break
+                    if len(x)==0:
+                        s_out.append('42')
+                        break
+                    else:
+                        #print(x[0][0])
+                        s_out.append(x[0][0])
+                        prefix+=' '+x[0][0]
+
+                print(' '.join([str(n) for n in s_out]))  
+        
+
+#--input_dir` путь к директории, в которой лежит коллекция документов. Если данный аргумент не задан, считать, что тексты вводятся из stdin.
 #--model`  путь к файлу, в который сохраняется модель.
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input-dir',  type=str)
+parser.add_argument('--input_dir',  type=str)
 parser.add_argument('--model', type=str, required=True)
 args = parser.parse_args()
 ####
 
+# раскоментить для сборки модели заново
+#M = NgramModel
+#M.fit(str(input_dir), str(model))
 
-f = open('../TinkoffML2022/data/detective/Era_Miloserdiya.txt', 'r', encoding="utf8")
-#список предложений
-lst_snt = []
-for s in f:
-    # Убрать последний символ '\n' из s
-    s = s.rstrip().lower()
-    if s!='':
-        snt = s.split('.')
-
-        # Вывести s для контроля
-        #print("s = ", s)
-
-        # Добавить строку s в список lst2
-    
-        lst_snt = lst_snt + [snt]
-
-output = []
-outcome = {}
-    
-for text in lst_snt:
-    text = str(text)
-
-#text = '- у тебя оружие с собой? у меня оружие при мне, а у тебя.'
-# генерирую список с n-граммами до 6
-    text = re.sub(r'[^а-яА-Я0-9\s]', ' ', text)
-    words = text.split()
-    
-    for WordsToCombine in range(1,7,1):
-        for i in range(len(words)- WordsToCombine+1):
-            output.append(words[i:i+WordsToCombine])
-    
-for k in output:
-    #print('k= ', k)
-    partial_outcome = output.count(k)
-    #print('freq=',partial_outcome)
-    s = ' '.join([str(n) for n in k])
-    LengthPrefix = len(k)
-    res=[]
-    for k2 in output:
-        if len(k2)== LengthPrefix+1:
-            if s == ' '.join([str(n) for n in k2[:LengthPrefix]]):
-                #print(k2[LengthPrefix:])
-                #print(k2[-1])
-                partial_outcome1 = output.count(k2)
-                #print('freq=',partial_outcome1)
-                if (k2[-1], partial_outcome1/partial_outcome) not in res:
-                    res.append((k2[-1], partial_outcome1/partial_outcome))
-    outcome[s] = res
-    #print(res)
-
-#print(outcome)       
-with open('../TinkoffML2022/model.pickle', 'wb') as handle:
-    pickle.dump(outcome, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
