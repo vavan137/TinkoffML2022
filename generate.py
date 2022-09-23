f3b8f8c724e076903ce01e81f39dc662d66504f0
vavan137 <113305894+vavan137@users.noreply.github.com>
2022-09-23 22:00:34 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import argparse
 import pickle
+from train import NgramModel
 
 #`--model` путь к файлу, из которого загружается модель.
 #`--prefix` необязательный аргумент. Начало предложения (одно или несколько слов). Если не указано, выбираем начальное слово случайно из всех слов.
@@ -11,29 +12,5 @@ parser.add_argument('--prefix', type=str, required=True)
 parser.add_argument('--length', type=str, required=True)
 args = parser.parse_args()
 ####
-#'../TinkoffML2022/model.pickle'
-with open(str(args.model), 'rb') as handle:
-    Model = pickle.load(handle)
 
-#читаем model.pcl
-prefix = str(args.prefix)
-the_length= int(args.length)
-s_out=[]
-
-for ix in range(the_length):
-    x = Model.get(prefix)
-    #print(x)
-# берем первый по порядку появления в тексте вариант
-    if x==None:
-        s_out.append('42')
-        #print("42")
-        break
-    if len(x)==0:
-        s_out.append('42')
-        break
-    else:
-        #print(x[0][0])
-        s_out.append(x[0][0])
-        prefix+=' '+x[0][0]
-
-print(' '.join([str(n) for n in s_out]))  
+NgramModel.predict(str(args.model), str(args.prefix), int(args.length))
